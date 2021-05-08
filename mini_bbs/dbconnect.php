094fdd3f462f8ab8ce4d2c3bbcbf73077ae66268
NS <70316815+nobuou0116@users.noreply.github.com>
2021-05-08 20:07:29 +0900

    Delete mini_bbs directory

--- mini_bbs/dbconnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-try {
-  $db = new PDO('mysql:dbname=mini_bbs;host=localhost;port:8889;charset=utf8','root','root');
-} catch(PDOExeption $e) {
-  print('DB接続エラー：'.$e->getMessage());
-}
-
-?>
